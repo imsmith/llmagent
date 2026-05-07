1e556b7b5fc1d8af8dd2ab185c7337c6eee3df1a
Ian Smith <197130+imsmith@users.noreply.github.com>
2026-05-07 14:33:28 -0700

    Add eden dependency for EDN wire format

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule LLMAgent.MixProject do
       {:req, "~> 0.5.0"},
       {:jason, "~> 1.4"},
       {:b58, "~> 1.0"},
+      {:eden, "~> 2.1"},
       {:comn, github: "imsmith/comn", tag: "v0.4.0"}
     ]
   end
